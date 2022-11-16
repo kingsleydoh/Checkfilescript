f0dffd7400b0fc7bd476b21d60d518bc8fc71b73
K.dohnwana <kingsleydoh@yahoo.com>
2022-11-15 20:26:05 -0600

    innitial code

---
+++ checkfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#virable set to check if file present
+FILE=~/inbound/Feed/text.txt
+
+#Script path to send email
+SCRIPT_PATH="/home/kingsley/email.sh"
+
+#check if the file is available
+if [ -f "$FILE" ]; then
+        exec "$SCRIPT_PATH"
+else
+        echo "$FILE do not exist. "
+fi
